b1242edf3082ab18b69ab09a9bb15b922a5b100a
jpchato <jpchato@gmail.com>
2020-07-13 13:56:29 -0700

    updated settings.py

--- task_manager_api_project/settings.py
+++ task_manager_api_project/settings.py
@@ -11,6 +11,17 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import environ
+
+env = environ.Env(
+    
+    DEBUG=(bool, False),
+    ENVIRONMENT=(str, 'PRODUCTION')
+)
+
+environ.Env.read_env()
+
+ENVIRONMENT = env.str('ENVIRONMENT')
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,12 +31,16 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '5uk&lz6c-bwg-+g@@@wa5em4ug6@(4#&+9th$aq(^_%*d@!71w'
+# SECRET_KEY = '5uk&lz6c-bwg-+g@@@wa5em4ug6@(4#&+9th$aq(^_%*d@!71w'
+SECRET_KEY = env.str('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = env.bool('DEBUG')
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS=['localhost','127.0.0.1']
+ALLOWED_HOSTS = tuple(env.list('ALLOWED_HOSTS'))
+
 
 
 # Application definition
@@ -36,17 +51,21 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    # 'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
 
     #3rd Party
     'rest_framework',
+    'corsheaders',
 
     # Local Apps
     'qstm.apps.QstmConfig',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -123,4 +142,20 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    STATIC_DIR,
+]
+
+STATIC_URL = '/static/'
+
+CORS_ORIGIN_WHITELIST = [
+    "http://localhost:3000",
+]
+
+CORS_ORIGIN_REGEX_WHITELIST = [
+    r"^http://localhost:3000$",
+
+]
